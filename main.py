3199a9f4e4dbb5e615cf0d2c1d0adf643e5da0a4
Алексей Тодорашко <149875136+LeshaTodorashko@users.noreply.github.com>
2024-03-30 14:26:11 +0200

    new massage

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+pygame.init()
+
+win_w = 800
+win_h = 700
+window = pygame.display.set_mode((win_w, win_h))
+fps = 60
+clock = pygame.time.Clock()
+
+pygame.mixer_music.load("snd/fon_sound.mp3")
+pygame.mixer_music.play(-1)
+pygame.mixer_music.set_volume(0.2)
+
+background = pygame.image.load("img/background.png")
+background = pygame.transform.scale(background, (win_w, win_h))
+
+
+game = True
+while game:
+    window.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    pygame.display.update()
+    clock.tick(fps)
